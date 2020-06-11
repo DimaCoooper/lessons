2c3986fee828a7e6a08f6d80182c19407577b737
DimaCoooper <39842006+DimaCoooper@users.noreply.github.com>
2020-06-11 12:10:02 +0300

    Update task_2.py

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -1,12 +1,8 @@
 raw_seconds = int(input("Введите время в секундах: "))
 
-for_h= 60 * 60
+hours = int(raw_seconds / 3600)
 
-hours = int(raw_seconds / for_h)
-print(hours)
-
-fot_m=int(raw_seconds % for_h)
-
+fot_m=int(raw_seconds % 3600)
 minutes = int(fot_m / 60)
 
 seconds = int(raw_seconds % 60)
