b63ca8942f51747f33d37dc8603e696a95090bf6
DimaCoooper <39842006+DimaCoooper@users.noreply.github.com>
2020-06-11 11:41:26 +0300

    Update task_1.py

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,1 +1,20 @@
+number_int = 1
+number_float = 1.5
+string = "Hello!"
 
+
+number1 = int(input("Введите число a: "))
+number2 = int(input("Введите число b: "))
+
+
+sum = str(number1 + number2)
+subtract = str(number1 - number2)
+mult = str(number1 * number2)
+division = str(number1 / number2)
+deg = str(number1 ** number2)
+
+print("a + b = " + sum)
+print("a - b = " + subtract)
+print("a / b = " + division)
+print("a * b = " + mult)
+print("a ** b = " + deg)
