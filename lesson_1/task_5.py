83396833406b241cbdf01cb468129329176f075a
Dima Russak <dmrussak@yandex-team.ru>
2020-06-11 13:29:31 +0300

    task5

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,7 +1,14 @@
-a = 3443
-b = 3443
+dohod = 6
+rashod = 3
 
-if a < b:
-    print(a)
-elif a > b:
-    
+if dohod < rashod:
+    diff = rashod - dohod
+    print("Убыток равен: " + str(diff))
+elif dohod == rashod:
+    print("вышли 0")
+else:
+    diff = dohod - rashod
+    print("рентабильность: {}".format(diff))
+    emp = int(input("Количество сотрудников: "))
+    diff_on_emp = diff / emp
+    print("Прибыль на одно сотрудника составляет {}.".format(diff_on_emp))
